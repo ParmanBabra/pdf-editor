a6aa153ff993c11f1bf4ceb63c86acafdd259b43
Nakarin Satayasai <nakarsat@scg.com>
2022-10-10 11:49:34 +0700

    add project

---
+++ src/Designer.tsx
@@ -0,0 +1,192 @@
+import { useEffect, useRef, useState } from "react";
+import { Designer, Template, checkTemplate } from "@pdfme/ui";
+import { generate } from "@pdfme/generator";
+import {
+  getTemplate,
+  readFile,
+  cloneDeep,
+  getTemplateFromJsonFile,
+  downloadJsonFile,
+} from "./helper";
+
+function App() {
+  const designerRef = useRef<HTMLDivElement | null>(null);
+  const designer = useRef<Designer | null>(null);
+  const [font, setFont] = useState<
+    Record<
+      string,
+      {
+        fallback?: boolean | undefined;
+        subset?: boolean | undefined;
+        data: ArrayBuffer | Uint8Array;
+      }
+    >
+  >();
+
+  async function getFont() {
+    const font = {
+      THSarabunNewBold: {
+        data: await fetch("fonts/THSarabunNew/THSarabunNew Bold.ttf").then(
+          (res) => res.arrayBuffer()
+        ),
+      },
+      THSarabunNewBoldItalic: {
+        data: await fetch(
+          "fonts/THSarabunNew/THSarabunNew BoldItalic.ttf"
+        ).then((res) => res.arrayBuffer()),
+      },
+      THSarabunNewItalic: {
+        data: await fetch("fonts/THSarabunNew/THSarabunNew Italic.ttf").then(
+          (res) => res.arrayBuffer()
+        ),
+      },
+      THSarabunNew: {
+        data: await fetch("fonts/THSarabunNew/THSarabunNew.ttf").then((res) =>
+          res.arrayBuffer()
+        ),
+        fallback: true,
+      },
+    };
+
+    setFont(font);
+    designer.current?.updateOptions({ font });
+
+    return font;
+  }
+
+  useEffect(() => {
+    if (designerRef.current) {
+      let template: Template = getTemplate();
+      try {
+        const templateString = localStorage.getItem("template");
+        const templateJson = templateString
+          ? JSON.parse(templateString)
+          : getTemplate();
+        checkTemplate(templateJson);
+        template = templateJson as Template;
+      } catch {
+        localStorage.removeItem("template");
+      }
+
+      designer.current = new Designer({
+        domContainer: designerRef.current,
+        template,
+      });
+      designer.current.onSaveTemplate(onSaveTemplate);
+    }
+
+    getFont();
+
+    return () => {
+      if (designer.current) {
+        designer.current.destroy();
+      }
+    };
+  }, [designerRef]);
+
+  const onChangeBasePDF = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      readFile(e.target.files[0], "dataURL").then(async (basePdf) => {
+        if (designer.current) {
+          designer.current.updateTemplate(
+            Object.assign(cloneDeep(designer.current.getTemplate()), {
+              basePdf,
+            })
+          );
+        }
+      });
+    }
+  };
+
+  const onLoadTemplate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files) {
+      getTemplateFromJsonFile(e.target.files[0])
+        .then((t) => {
+          if (designer.current) {
+            designer.current.updateTemplate(t);
+          }
+        })
+        .catch((e) => {
+          alert(`Invalid template file.
+--------------------------
+${e}`);
+        });
+    }
+  };
+
+  const onDownloadTemplate = () => {
+    if (designer.current) {
+      downloadJsonFile(designer.current.getTemplate(), "template");
+    }
+  };
+
+  const onSaveTemplate = (template?: Template) => {
+    if (designer.current) {
+      localStorage.setItem(
+        "template",
+        JSON.stringify(template || designer.current.getTemplate())
+      );
+      alert("Saved!");
+    }
+  };
+
+  const onResetTemplate = () => {
+    if (designer.current) {
+      designer.current.updateTemplate(getTemplate());
+      localStorage.removeItem("template");
+    }
+  };
+
+  const onGeneratePDF = async () => {
+    if (designer.current) {
+      const template = designer.current.getTemplate();
+      const inputs = template.sampledata ?? [];
+      const pdf = await generate({ template, inputs, options: { font } });
+      const blob = new Blob([pdf.buffer], { type: "application/pdf" });
+      window.open(URL.createObjectURL(blob));
+    }
+  };
+
+  return (
+    <div>
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <strong>Designer</strong>
+        <span style={{ margin: "0 1rem" }}>:</span>
+        <label style={{ width: 180 }}>
+          Change BasePDF
+          <input
+            type="file"
+            accept="application/pdf"
+            onChange={onChangeBasePDF}
+          />
+        </label>
+        <span style={{ margin: "0 1rem" }}>/</span>
+        <label style={{ width: 180 }}>
+          Load Template
+          <input
+            type="file"
+            accept="application/json"
+            onChange={onLoadTemplate}
+          />
+        </label>
+        <span style={{ margin: "0 1rem" }}>/</span>
+        <button onClick={onDownloadTemplate}>Download Template</button>
+        <span style={{ margin: "0 1rem" }}>/</span>
+        <button onClick={() => onSaveTemplate()}>Save Template</button>
+        <span style={{ margin: "0 1rem" }}>/</span>
+        <button onClick={onResetTemplate}>Reset Template</button>
+        <span style={{ margin: "0 1rem" }}>/</span>
+        <button onClick={onGeneratePDF}>Generate PDF</button>
+      </header>
+      <div ref={designerRef} />
+    </div>
+  );
+}
+
+export default App;
